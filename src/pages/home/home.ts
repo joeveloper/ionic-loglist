e1384711e8584f3f5010ee3384c8b30db23518fc
joeveloper <bebosir@gmail.com>
2018-12-01 23:00:06 +0100

    initial commit on all files

---
+++ src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+import { NavController, App } from "ionic-angular";
+
+@Component({
+  selector: "page-home",
+  templateUrl: "home.html"
+})
+export class HomePage {
+  constructor(public navCtrl: NavController, public app: App) {}
+
+  logout() {
+    const root = this.app.getRootNav();
+    root.popToRoot();
+  }
+}
