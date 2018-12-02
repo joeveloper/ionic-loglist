3f2319e426a2b435382f4354afa3bb5bc7dfea8b
joeveloper <bebosir@gmail.com>
2018-12-02 16:22:46 +0100

    brief adjustment to login behaviour

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,10 @@
 import { Component } from "@angular/core";
-import { NavController } from "ionic-angular";
+import {
+  NavController,
+  App,
+  AlertController,
+  ToastController
+} from "ionic-angular";
 import { WelcomePage } from "../welcome/welcome";
 
 @Component({
@@ -7,9 +12,48 @@ import { WelcomePage } from "../welcome/welcome";
   templateUrl: "home.html"
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {}
+  constructor(
+    public navCtrl: NavController,
+    private app: App,
+    private alerter: AlertController,
+    private toaster: ToastController
+  ) {}
 
   logout() {
-    this.navCtrl.push(WelcomePage);
+    //Presents a confirmation dialog
+    this.presentConfirm("Wonna Log out?", "Are you sure?", () => {
+      this.toaster
+        .create({
+          message: "Logout success",
+          duration: 2000
+        })
+        .present();
+      this.app.getRootNav().setRoot(WelcomePage);
+    });
+    //this.navCtrl.push(WelcomePage);
+  }
+
+  //A simple function to present confirmation dialogue
+  private presentConfirm(title: string, message: string, handler: Function) {
+    let alert = this.alerter.create({
+      title: title,
+      message: message,
+      buttons: [
+        {
+          text: "Cancel",
+          role: "cancel",
+          handler: () => {
+            //
+          }
+        },
+        {
+          text: "Yes",
+          handler: () => {
+            handler();
+          }
+        }
+      ]
+    });
+    alert.present();
   }
 }
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,5 +1,5 @@
-import { Component } from "@angular/core";
-import { IonicPage, NavController, NavParams } from "ionic-angular";
+import { Component, ViewChild } from "@angular/core";
+import { IonicPage, NavController, NavParams, LoadingController, Loading, ToastController, TextInput } from "ionic-angular";
 import { TabsPage } from "../tabs/tabs";
 
 /**
@@ -15,13 +15,50 @@ import { TabsPage } from "../tabs/tabs";
   templateUrl: "login.html"
 })
 export class LoginPage {
-  constructor(public navCtrl: NavController, public navParams: NavParams) {}
+
+  //Login Data::
+  //username = joe
+  //password = password
+
+  @ViewChild('username') public username: TextInput;
+  @ViewChild('password') public password: TextInput;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private loadingCtrl: LoadingController, private toaster: ToastController) {}
 
   ionViewDidLoad() {
     console.log("ionViewDidLoad LoginPage");
   }
 
   login() {
-    this.navCtrl.push(TabsPage);
+    let loading: Loading;
+    loading = this.loadingCtrl.create({
+      content: 'Signing you in, please wait...'
+    });
+    loading.present();
+
+    //Simulating authentication
+    if(this.username.value === 'joe' && this.password.value === 'password'){
+      //Simulate login in to server.
+      setTimeout(() => {
+        loading.dismiss();
+        this.toaster.create({
+          message: 'Login Success!',
+          duration: 2000
+        }).present();
+        this.navCtrl.push(TabsPage);
+      }, 3000);
+    }
+    else{
+      setTimeout(()=>{
+        loading.dismiss();
+        this.toaster.create({
+          message: 'Login Failed..invalid credentials',
+          duration: 2000
+        }).present();
+      }, 3000)
+    }
   }
+
+
 }
+
